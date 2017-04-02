fef4e35b8011385469f508389465a27abb4cc8f0
Antonio Valentino <antonio.valentino@alliance-healthcare.net>
2017-04-02 17:18:53 +0200

    updated file.js

--- file/file.js
+++ file/file.js
@@ -1,5 +1,6 @@
 var fs=require('fs')
 
+// fs.open(path, flags[, mode], callback)
 fs.open(
 	'info.txt','r',
 	function(err, handle){
@@ -9,6 +10,7 @@ fs.open(
 			return;
 		}
 		var buf=new Buffer(100000);
+		//fs.read(fd, buffer, offset, length, position, callback)
 		fs.read(
 			handle,buf,0,100000,null,
 			function(err,length){
@@ -18,6 +20,7 @@ fs.open(
 					return;
 				}
 				console.log(buf.toString('ascii',0,length));
+				//fs.close(fd, callback)
 				fs.close(handle, function() { /* don't do anything */});
 			}
 		);
